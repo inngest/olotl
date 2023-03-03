235b49f43f7f591d079e86b964c3d64add3c7010
Tony Holdstock-Brown <tonyhb@gmail.com>
2023-03-03 14:44:08 -0800

    Remove cronjob

--- inngest/cron.ts
+++
@@ -1,9 +0,0 @@
-import { inngest } from "./client";
-
-export const cronjob = inngest.createFunction(
-  { name: "test cron" },
-  { cron: "* * * * *" },
-  () => {
-    return { status: 200, v: 4 };
-  }
-);
--- pages/api/inngest.ts
+++ pages/api/inngest.ts
@@ -1,13 +1,12 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../inngest/client";
-import { cronjob } from "../../inngest/cron";
 import { newPR } from "../../inngest/new_pr";
 import { review, reviewComment } from "../../inngest/pr_review";
 
 // You must export the serve handler, which hosts all of the provided functions
 // under one API endpoint.
 const api = (req: any, res: any) => {
-  const fn = serve(inngest, [newPR, reviewComment, review, cronjob]);
+  const fn = serve(inngest, [newPR, reviewComment, review]);
 
   console.log(req.headers);
 
